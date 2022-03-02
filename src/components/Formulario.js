a126ab6532be6ff49b276bde809f01734d38c12d
Carlos <carlos.anezeh@gmail.com>
2022-03-02 15:05:26 -0600

    Fix issues with the news component.
    Added a new favicon.

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,38 +1,55 @@
-import React from 'react'
+import React from 'react';  
 import styles from './Formulario.module.css';
+import PropTypes from 'prop-types';
 import useSelect from '../hooks/useSelect';
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
 
-  const OPCIONES = [
-    {value: 'general', label: 'General'},
-    {value: 'business', label: 'Bussines'},
-    {value: 'entertainment', label: 'Entertainment'},
-    {value: 'health', label: 'Health'},
-    {value: 'science', label: 'Science'},
-    {value: 'sports', label: 'Sports'},
-    {value: 'technology', label: 'Technology'}
-  ];
+    const OPCIONES = [
+        { value: 'general', label: 'General'},
+        { value: 'business', label: 'Business'},
+        { value: 'entertainment', label: 'Entretenimient'},
+        { value: 'health', label: 'Health'},
+        { value: 'science', label: 'Science'},
+        { value: 'sports', label: 'Sports'},
+        { value: 'technology', label: 'Technology'},
+    ]
 
-  const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
-  return (
-    <div className={`${styles.buscador} row`}>
-      <div className="col s12 m8 offset-m2">
-        <form>
-          <h2 className={styles.heading}>Search news by category</h2>
-          <SelectNoticias />
-          <div className="input-field col s12">
-            <input
-              type="submit"
-              id="autocomplete-input"
-              className={`${styles.btn_block} btn-large amber darken-2`}
-              value="Search"
-            />
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    // utilizar custom hook
+    const [ categoria, SelectNoticias ] = useSelect('general', OPCIONES);
+
+    // submit al form, pasar categoria a app.js
+    const buscarNoticias = e => {
+        e.preventDefault();
+
+        guardarCategoria(categoria);
+    }
+
+    return (
+        <div className={`${styles.buscador} row`}>
+            <div className="col s12 m8 offset-m2">
+                <form
+                    onSubmit={buscarNoticias}
+                >
+                    <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+                    <SelectNoticias />
+
+                    <div className="input-field col s12">
+                        <input
+                            type="submit"
+                            className={`${styles['btn-block']}  btn-large amber darken-2`}
+                            value="Search"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
+Formulario.propTypes = {
+    guardarCategoria: PropTypes.func.isRequired
+}
+ 
 export default Formulario;
--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -1,29 +1,23 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react';
 
 const useSelect = (stateInicial, opciones) => {
-  
-  // stateInicial es el valor inicial del state
-  // opciones es un array de objetos con las opciones del select
-  // [{value: '', label: ''}]
-  // value es el valor del option
-  // label es el texto que se muestra en el option
-  const [state, actualizarState] = useState('');
 
-  const SelectNoticias = () => (
-    <select
-      className="browser-default"
-      value={state}
-      onChange={e => actualizarState(e.target.value)}
-    >
-      <option value="">News by category...</option>
-      {opciones.map(opcion => (
-        <option key={opcion.value} value={opcion.value}>
-          {opcion.label}
-        </option>
-      ))}
-    </select>
-  )
-  return [state, SelectNoticias]; // [state, funcion]
+    // state del custom hook
+    const [state, actualizarState] = useState(stateInicial);
+
+    const SelectNoticias =  () => (
+        <select
+            className="browser-default"
+            value={state}
+            onChange={e => actualizarState(e.target.value)}
+        >
+            {opciones.map(opcion => (
+                <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
+            ))}
+        </select>
+    );
+
+    return [state, SelectNoticias];
 }
-
+ 
 export default useSelect;
