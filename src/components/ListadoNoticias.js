ddff8df32764c795375c60bb2fb8e98e94a5a35e
Carlos <carlos.anezeh@gmail.com>
2022-03-02 15:21:09 -0600

    Add a readme file.

--- src/components/ListadoNoticias.js
+++ src/components/ListadoNoticias.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Noticia from './Noticia';
 import PropTypes from 'prop-types'
 
-const ListadoNoticias = ({noticias}) =>  ( 
+const ListadoNoticias = ({noticias}) =>  (
         <div className="row">
-            {noticias.map(noticia => (
-                <Noticia 
+            { noticias.map( noticia => (
+                <Noticia
                     key={noticia.url}
                     noticia={noticia}
                 />
-            ))}
+            )) }
         </div>
 );
 
